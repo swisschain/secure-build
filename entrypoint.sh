5b0c2507027bd8d3f6dcf4945bbbd565bd578dcb
Dvin <dvyu@yandex.ru>
2022-04-29 14:23:56 +0500

    test_14

--- entrypoint.sh
+++ entrypoint.sh
@@ -358,6 +358,10 @@ EOF
 echo build script output
 ./build.py instance-env --env sbs-config.json 2>&1
 echo build script output
+./build.py instance-env --env sbs-config.json 2>&1|sed s/\-e\ /\\n\\n/g
+echo build script output
+for i in $(./build.py instance-env --env sbs-config.json 2>&1|sed s/\-e\ /\\n\\n/g|grep "CLIENT_CRT\|CLIENT_CA\|SERVER_CRT\|SERVER_KEY") ;do printf "%s %s " "-e" $i; done
+echo build script output
 #server_key=$(./build.py instance-env --env sbs-config.json 2>&1|sed s/-e\ /\\n\\n/g|grep "SERVER_KEY")
 #ibmcloud hpvs instance-create $BUILD_SERVER free fra05 --rd-path "secure_build.asc" -i 1.3.0.4 -e $certs
 #ibmcloud hpvs instance-create $BUILD_SERVER free fra05 --rd-path "secure_build.asc" -i 1.3.0.4 -e ${client_crt} -e ${client_ca} -e ${server_crt} -e ${server_key}
