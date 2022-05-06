6f926b719526aa392ae1260baf4a607176ec9c87
Sergey Korolev <sergey@korolevlive.com>
2022-05-06 08:40:17 +0300

    test_15

--- entrypoint.sh
+++ entrypoint.sh
@@ -390,7 +390,7 @@ deploy_tag=`./build.py log --log build --env sbs-config.json |grep image_tag|awk
 ./build.py get-manifest --env sbs-config.json  --verify-manifest
 tag_out=$(./build.py log --log build --env sbs-config.json)
 echo tag_out=$tag_out
-image_tag=$(echo $tag_out | |grep image_tag|awk -F- '{print $5}')
+image_tag=$(echo $tag_out | grep image_tag|awk -F- '{print $5}')
 echo image_tag=$image_tag
 cat << EOF > pass.sh
 ./build.py get-config-json --env sbs-config.json --key-id $CONTAINER_NAME-$image_tag
