276211a2200d6fd0315e41add14ea5b93ff60dc6
Sergey Korolev <sergey@korolevlive.com>
2022-05-06 12:41:41 +0300

    test_15

--- entrypoint.sh
+++ entrypoint.sh
@@ -407,9 +407,11 @@ DONE
 
 #Cleaning up orphan resources
 
-#ibmcloud hpvs instance-delete $BUILD_SERVER -f
-#reclamation_id=`ibmcloud resource reclamations|grep SCHEDULED|awk '{print $1}'
-#ibmcloud resource reclamation-delete $reclamation_id -f
+ibmcloud hpvs instance-delete $BUILD_SERVER -f
+sleep 10
+reclamation_id=`ibmcloud resource reclamations|grep SCHEDULED|awk '{print $1}'
+ibmcloud resource reclamation-delete $reclamation_id -f
+ibmcloud cr namespace-rm $CR_NAME
 
 #Artifacts
 
