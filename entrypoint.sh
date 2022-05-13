529dd114cca14554a548932183e8f55cf4819f58
Dvin <dvyu@yandex.ru>
2022-05-13 17:05:55 +0500

    test_15

--- entrypoint.sh
+++ entrypoint.sh
@@ -392,8 +392,12 @@ tag_out=$(./build.py log --log build --env sbs-config.json|grep image_tag|awk -F
 echo tag_out=$tag_out
 image_tag=$(echo $tag_out)
 echo image_tag=$image_tag
+echo PWD
+pwd
 cat << EOF > pass.sh
 export GPG_TTY=\$(tty)
+echo PWD
+pwd
 ./build.py get-config-json --env sbs-config.json --key-id $CONTAINER_NAME-dd9bff2 > out.log 2>&1
 cat out.log
 EOF
